344ce7137de64f0d2207d89c77c26c4f46b4be55
turik777 <52492191+turik777@users.noreply.github.com>
2024-06-01 02:07:31 +0300

    Add listeners to show displays

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
 import "./style.css";
+import { addProject } from "./addTodo";
+
+const showProjectDialog = document.querySelector(".projects button");
+const projectDialog = document.querySelector(".new-project");
+const addProjectButton = document.querySelector(".new-project > input");
+const projectTitle = document.getElementById("project-title");
+
+const showTodoDialog = document.querySelector(".content button");
+const todoDialog = document.querySelector(".new-todo");
+const addTodoButton = document.querySelector(".new-todo > input");
+
+showProjectDialog.addEventListener("click", () => {
+    projectDialog.style.display = "block";
+})
+
+addProjectButton.addEventListener("click", () => {
+    addProject(projectTitle.value);
+    projectDialog.style.display = "none";
+    projectTitle.value = "";
+})
+
+showTodoDialog.addEventListener("click", () => {
+    todoDialog.style.display = "block";
+})
+
+addTodoButton.addEventListener("click", () => {
+    todoDialog.style.display = "none";
+})
