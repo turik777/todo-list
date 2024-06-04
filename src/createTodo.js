a85674eaa8c09b5e387224d9267a43bd292a92fa
turik777 <52492191+turik777@users.noreply.github.com>
2024-06-05 02:15:33 +0300

    Refactor createTodo and add listeners to buttons

--- src/createTodo.js
+++ src/createTodo.js
@@ -2,122 +2,83 @@ import { projectList } from "./addTodo";
 
 export const todos = document.querySelector(".todos");
 
+function createTodo(element) {
+    const todo = document.createElement("div");
+    todo.classList.add("todo");
+    const div = document.createElement("div");
+    const todoTitle = document.createElement("div");
+    todoTitle.classList.add("todo-title");
+    const todoDescription = document.createElement("div");
+    todoDescription.classList.add("todo-description");
+    const todoDate = document.createElement("div");
+    todoDate.classList.add("todo-date");
+    const divDelete = document.createElement("div");
+    const todoDeleteButton = document.createElement("button");
+    const deleteImage = document.createElement("img");
+    deleteImage.src = "/src/img/delete-icon.svg";
+    
+    todoTitle.textContent = element.todoTitle;
+    todoDescription.textContent = element.description;
+    todoDate.textContent = element.dueDate;
+    const todoProject = element.project;
+
+    switch (element.priority) {
+        case "1":
+            todo.style.borderLeft = "1em solid green";
+            break;
+        case "2":
+            todo.style.borderLeft = "1em solid orange";
+            break;
+        case "3":
+            todo.style.borderLeft = "1em solid red";
+            break;
+        default:
+            break;
+    }
+
+    todos.appendChild(todo);
+    todo.appendChild(div);
+    todo.appendChild(divDelete);
+    div.appendChild(todoTitle);
+    div.appendChild(todoDate);
+    divDelete.appendChild(todoDescription);
+    divDelete.appendChild(todoDeleteButton);
+    todoDeleteButton.appendChild(deleteImage);
+
+    todoDeleteButton.addEventListener("click", () => {
+        const projectIndex = projectList.findIndex(project => project.projectTitle === todoProject || project.todoTitle === todoTitle.textContent);
+
+        if (projectList[projectIndex].projectTitle) {
+            const todoIndex = projectList[projectIndex].todoList.findIndex(todo => todo.todoTitle === todoTitle.textContent);
+
+            projectList[projectIndex].todoList.splice(todoIndex, 1);
+        } else {
+            projectList.splice(projectIndex, 1);
+        }
+
+        todo.remove();
+    })
+}
+
 export function createTodoDOM(projectName) {
     todos.innerHTML = "";
 
-    projectList.forEach(project => {
-        const todo = document.createElement("div");
-        todo.classList.add("todo");
-        const div = document.createElement("div");
-        const todoTitle = document.createElement("div");
-        todoTitle.classList.add("todo-title");
-        const todoDescription = document.createElement("div");
-        todoDescription.classList.add("todo-description");
-        const todoDate = document.createElement("div");
-        todoDate.classList.add("todo-date");
-
-        if (projectName === project.projectTitle) {
-            project.todoList.forEach(element => {
-                const todo = document.createElement("div");
-                todo.classList.add("todo");
-                const div = document.createElement("div");
-                const todoTitle = document.createElement("div");
-                todoTitle.classList.add("todo-title");
-                const todoDescription = document.createElement("div");
-                todoDescription.classList.add("todo-description");
-                const todoDate = document.createElement("div");
-                todoDate.classList.add("todo-date");
-
-                todoTitle.textContent = element.todoTitle;
-                todoDescription.textContent = element.description;
-                todoDate.textContent = element.dueDate;
-
-                switch (element.priority) {
-                    case "1":
-                        todo.style.borderLeft = "1em solid green";
-                        break;
-                    case "2":
-                        todo.style.borderLeft = "1em solid orange";
-                        break;
-                    case "3":
-                        todo.style.borderLeft = "1em solid red";
-                        break;
-                    default:
-                        break;
-                }
-
-                todos.appendChild(todo);
-                todo.appendChild(div);
-                todo.appendChild(todoDescription);
-                div.appendChild(todoTitle);
-                div.appendChild(todoDate);
+    projectList.forEach(element => {
+        if (projectName === element.projectTitle) {
+            element.todoList.forEach(element => {
+                createTodo(element);
             });
         };
 
         if (projectName === "All todos") {    
-            if (project.projectTitle) {
-                project.todoList.forEach(element => {
-                    const todo = document.createElement("div");
-                    todo.classList.add("todo");
-                    const div = document.createElement("div");
-                    const todoTitle = document.createElement("div");
-                    todoTitle.classList.add("todo-title");
-                    const todoDescription = document.createElement("div");
-                    todoDescription.classList.add("todo-description");
-                    const todoDate = document.createElement("div");
-                    todoDate.classList.add("todo-date");
-                    
-                    todoTitle.textContent = element.todoTitle;
-                    todoDescription.textContent = element.description;
-                    todoDate.textContent = element.dueDate;
-    
-                    switch (element.priority) {
-                        case "1":
-                            todo.style.borderLeft = "1em solid green";
-                            break;
-                        case "2":
-                            todo.style.borderLeft = "1em solid orange";
-                            break;
-                        case "3":
-                            todo.style.borderLeft = "1em solid red";
-                            break;
-                        default:
-                            break;
-                    }
-
-                    todos.appendChild(todo);
-                    todo.appendChild(div);
-                    todo.appendChild(todoDescription);
-                    div.appendChild(todoTitle);
-                    div.appendChild(todoDate);
+            if (element.projectTitle) {
+                element.todoList.forEach(element => {
+                    createTodo(element);
                 });
             } else {
-                todoTitle.textContent = project.todoTitle;
-                todoDescription.textContent = project.description;
-                todoDate.textContent = project.dueDate;
-    
-                switch (project.priority) {
-                    case "1":
-                        todo.style.borderLeft = "1em solid green";
-                        break;
-                    case "2":
-                        todo.style.borderLeft = "1em solid orange";
-                        break;
-                    case "3":
-                        todo.style.borderLeft = "1em solid red";
-                        break;
-                    default:
-                        break;
-                }
-
-                todos.appendChild(todo);
-                todo.appendChild(div);
-                todo.appendChild(todoDescription);
-                div.appendChild(todoTitle);
-                div.appendChild(todoDate);
+                createTodo(element);
             }
         };
-
     });
 }
 
