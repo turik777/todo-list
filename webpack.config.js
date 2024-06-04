dafd29b501390eaf7290658638f0f949647be565
turik777 <52492191+turik777@users.noreply.github.com>
2024-06-05 02:09:21 +0300

    Add inline source map

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     mode: "production",
     entry: './src/index.js',
+    devtool: "inline-source-map",
     plugins: [
         new HtmlWebpackPlugin({
             template: "./src/index.html",
