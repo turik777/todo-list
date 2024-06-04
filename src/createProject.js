ff6f658bcce834ba483c0dbc1b8cae14059f06f0
turik777 <52492191+turik777@users.noreply.github.com>
2024-06-05 02:12:53 +0300

    Add listeners to project delete buttons

--- src/createProject.js
+++ src/createProject.js
@@ -8,15 +8,33 @@ export function createProjectDOM() {
     sidebar.innerHTML = "";
 
     projectList.forEach(project => {
+        const div = document.createElement("div");
         const projectButton = document.createElement("button");
+        const projectDeleteButton = document.createElement("button");
+        const deleteImage = document.createElement("img");
 
         projectButton.textContent = project.projectTitle;
-        if (project.projectTitle) sidebar.appendChild(projectButton);
+        if (project.projectTitle) {
+            sidebar.appendChild(div);
+            div.appendChild(projectButton);
+            div.appendChild(projectDeleteButton);
+            projectDeleteButton.appendChild(deleteImage);
+            deleteImage.src = "/src/img/delete-icon.svg";
+        }
 
         projectButton.addEventListener("click", () => {
             contentTitle.textContent = project.projectTitle;
 
             createTodoDOM(project.projectTitle);
+        });
+
+        projectDeleteButton.addEventListener("click", () => {
+            const projectIndex = projectList.findIndex(project => 
+                project.projectTitle === projectButton.textContent);
+            projectList.splice(projectIndex, 1);
+
+            createTodoDOM(project.projectTitle);
+            div.remove();
         })
     });
 
