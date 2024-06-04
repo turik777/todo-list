83cbfaf63be04cfbf35b0fd771eccdc0a81b5af3
turik777 <52492191+turik777@users.noreply.github.com>
2024-06-04 18:22:39 +0300

    Delete addProjectTodo function

--- src/addTodo.js
+++ src/addTodo.js
@@ -16,19 +16,18 @@ class Project {
     }
 }
 
-export function addTodo(todoTitle, description, dueDate, priority) {
-    const todo = new Todo(todoTitle, description, dueDate, priority);
-    projectList.push(todo);
-}
-
 export function addProject(projectTitle) {
     const project = new Project(projectTitle);
     projectList.push(project);
 }
 
-export function addProjectTodo(todoTitle, description, dueDate, priority, projectTitle) {
+export function addTodo(todoTitle, description, dueDate, priority, projectTitle) {
     const todo = new Todo(todoTitle, description, dueDate, priority);
     const project = projectList.find(project => 
         project.projectTitle === projectTitle);
-    project.todoList.push(todo);
+    if (projectTitle === "All todos") { 
+        projectList.push(todo);
+    } else if (projectTitle) {
+        project.todoList.push(todo);
+    }
 }
