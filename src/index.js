6bfd9c845ca69029fadb31f667eba83aafb0fe6e
turik777 <52492191+turik777@users.noreply.github.com>
2024-06-04 18:24:03 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
 import "./style.css";
-import { addProject } from "./addTodo";
-import { createProjectDOM } from "./createProject";
+import { projectList, addProject, addTodo } from "./addTodo";
+import { createProjectDOM, contentTitle } from "./createProject";
+import { createTodoDOM } from "./createTodo";
+
+const todosButton = document.getElementById("todos-button");
 
 const showProjectDialog = document.querySelector(".projects button");
 const projectDialog = document.querySelector(".new-project");
 const addProjectButton = document.querySelector(".new-project > input");
 const projectTitle = document.getElementById("project-title");
+const title = document.getElementById("title");
 
 const showTodoDialog = document.querySelector(".content button");
 const todoDialog = document.querySelector(".new-todo");
 const addTodoButton = document.querySelector(".new-todo > input");
+const todoTitle = document.getElementById("todo-name");
+const todoDescription = document.getElementById("todo-description");
+const todoDate = document.getElementById("todo-date");
+const todoPriority = document.getElementById("todo-priority");
+
+todosButton.addEventListener("click", () => {
+    createTodoDOM("All todos");
+    contentTitle.textContent = "All todos";
+});
 
 showProjectDialog.addEventListener("click", () => {
     projectDialog.style.display = "block";
@@ -27,5 +40,13 @@ showTodoDialog.addEventListener("click", () => {
 })
 
 addTodoButton.addEventListener("click", () => {
+    addTodo(todoTitle.value, todoDescription.value, todoDate.value, todoPriority.value, title.textContent);
+    createTodoDOM(title.textContent);
     todoDialog.style.display = "none";
+    todoTitle.value = "";
+    todoDescription.value = "";
+    todoDate.value = "";
+    todoPriority.value = "";
 })
+
+console.log(projectList);
